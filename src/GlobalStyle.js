74b905b56b10a828f31632d7698d28b487b69769
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-03-22 19:01:11 +0100

    Initial commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: sans-serif;
+    font-size: 112.5%;
+  }
+`;
