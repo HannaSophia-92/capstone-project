54a6edf93ad944553797da8a1e7ab1fca62b87dd
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-04-12 09:45:47 +0200

    Add message to home page if no past trip is created yet

--- src/components/PastTripList/PastTripList.js
+++ src/components/PastTripList/PastTripList.js
@@ -14,6 +14,9 @@ export default function PastTripList({
 }) {
   const [isActive, setIsActive] = useState(true);
 
+  if (!history || history.length === 0) {
+    return <Message>Seems like you don't have any past trips yet.</Message>;
+  }
   return (
     <>
       {isActive && (
@@ -94,5 +97,10 @@ const Card = styled.ul`
   flex-direction: column;
   gap: 20px;
   list-style: none;
+  margin: 20px;
+`;
+
+const Message = styled.p`
+  text-align: center;
   margin: 20px;
 `;
